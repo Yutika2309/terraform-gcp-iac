e134506e8c0f50789c9878c2f45ce0759b14c36e
Yutika2309 <yutikarege2309@gmail.com>
2025-05-12 20:12:02 +0530

    trial without terraform plan

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Provisioning Services as Code (IaC - Infrastructure as Code):
+# Provisioning Services as Code (IaC - Infrastructure as Code)
+
+Important links:
+
+Inference code for https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/storage_bucket
---
+++ main.tf
@@ -0,0 +1,7 @@
+resource "google_storage_bucket" "bucket-1" {
+  name                     = "yutikas-demo-bucket"
+  project                  = "terraform-personal-project"
+  location                 = "US"
+  force_destroy            = true
+  public_access_prevention = "enforced"
+}
